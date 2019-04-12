b13bc5942e09f548253260de83e80c3d2171b3ab
JamesBrazier <49045066+JamesBrazier@users.noreply.github.com>
2019-04-12 21:03:18 +1200

    Update Link.py
    Added Commenting to file

--- Link.py
+++ Link.py
@@ -9,9 +9,9 @@ class Link:
     This class simply holds the data for a link so it can be indexed more readably
     """
     def __init__(self):
-        self.port = -1
-        self.metric = -1
-        self.routerID = -1
+        self.port = -1 #the port this link is on
+        self.metric = -1 #the cost of the link
+        self.routerID = -1 #the ID of the router on the otherside
 
     def __str__(self):
         return "(Link: port = {0}, metric = {1}, routerID = {2})".format(self.port, self.metric, self.routerID)
